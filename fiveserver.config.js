a666b4456c1974eef37f2d0e62d99d9a0edc890f
illeesam <illeesam@gmail.com>
2024-05-16 07:24:22 +0000

    dd

---
+++ fiveserver.config.js
@@ -0,0 +1,11 @@
+// fiveserver.config.js
+module.exports = {
+    port: 8080, // 8080 -> 50180
+    highlight: true, // 하이라이트 기능 활성화
+    injectBody: true, // 즉시 업데이트 활성화
+    remoteLogs: true, // 원격 로그 활성화
+    remoteLogs: "yellow", // 원격 로그 활성화 하고 노라색을사용합니다.
+    injectCss: true, // CSS 주입을비활성화합니다.
+    navigate: true, // 자동탐색 활성화
+    // root: 'src',
+  };
